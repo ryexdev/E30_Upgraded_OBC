2265630cd4dd51f362471cb5cd52efd06fbe276b
ryanredbaron <ryanredbaroN@gmail.com>
2021-03-09 14:42:14 -0800

    Update V1-PZ_GP.py

--- V1-PZ_GP.py
+++ V1-PZ_GP.py
@@ -57,7 +57,7 @@ def hdat_Pressed():
 def mindat_Pressed():
    global MainTextMode
    MainTextMode = 'mindat'
-   player.communicate(b'n')[0]
+   player.stdin.write('h')
 
 def Hour_Pressed():
    global MainTextMode
@@ -78,7 +78,7 @@ def Memo_Pressed():
    global player
    MainTextMode = 'memo'
    filelist = glob('/home/pi/Music/F2/*.mp3')
-   player = Popen(["mpg123", "-z", "--list"] + filelist, stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+   player = Popen(["mpg123", "-z", "--list"] + filelist, stdin=PIPE, shell=True)
 
 def TrackMode_Pressed():
    OBC.hide()
