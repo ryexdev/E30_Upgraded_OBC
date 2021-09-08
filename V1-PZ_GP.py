d6c57beb6847e6dbd54ef94ba6c5853b7503efd2
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2021-09-07 23:22:54 -0700

    Update V1-PZ_GP.py

--- V1-PZ_GP.py
+++ V1-PZ_GP.py
@@ -284,7 +284,7 @@ OBCSpacing = 1
 OBC = App(title="OBC", width=480, height=600, layout="grid")
 OBC.bg = "black"
 OBC.full_screen = True
-OBCMainText = Text(OBC, text = "Loading", font="digital-7", width=11, height="2", size=69, color="orange", grid=[0,OBCSpacing]);OBCSpacing += 1;
+OBCMainText = Text(OBC, text = "Loading", font="digital-7", width=11, height="4", size=69, color="orange", grid=[0,OBCSpacing]);OBCSpacing += 1;
 OBCMainText.bg = "#5E0000"
 OBCMainText.repeat(250, OBC_Data)
 spacer = Text(OBC, text="", grid=[0,OBCSpacing]);OBCSpacing += 1;
@@ -294,21 +294,21 @@ spacer = Text(OBC, text="", grid=[0,OBCSpacing]);OBCSpacing += 1;
 OBChdat = PushButton(OBC, command=hdat_Pressed, text="Speed                          ", align="left", height="6", width="fill", grid=[0,OBCSpacing])
 OBChdat.bg = "black"
 OBChdat.text_color = "white"
-OBCmindat = PushButton(OBC, command=Temp_Pressed, text="Temperature", align="right", height="6", width="fill", grid=[0,OBCSpacing]);OBCSpacing += 1;
+OBCmindat = PushButton(OBC, command=Temp_Pressed, text="               Temperature", align="right", height="6", width="fill", grid=[0,OBCSpacing]);OBCSpacing += 1;
 OBCmindat.bg = "black"
 OBCmindat.text_color = "white"
 spacer = Text(OBC, text="", grid=[0,OBCSpacing]);OBCSpacing += 1;
 OBChour = PushButton(OBC, command=Hour_Pressed, text="Hour                           ", align="left", height="6", width="fill", grid=[0,OBCSpacing])
 OBChour.bg = "black"
 OBChour.text_color = "white"
-OBCdate = PushButton(OBC, command=Date_Pressed, text="Date", align="right", height="6", width="fill", grid=[0,OBCSpacing]);OBCSpacing += 1;
+OBCdate = PushButton(OBC, command=Date_Pressed, text="                            Date", align="right", height="6", width="fill", grid=[0,OBCSpacing]);OBCSpacing += 1;
 OBCdate.bg = "black"
 OBCdate.text_color = "white"
 spacer = Text(OBC, text="", grid=[0,OBCSpacing]);OBCSpacing += 1;
 OBCtemp = PushButton(OBC, command=mindat_Pressed, text="Update                       ", align="left", height="6", width="fill", grid=[0,OBCSpacing])
 OBCtemp.bg = "black"
 OBCtemp.text_color = "white"
-OBCmemo = PushButton(OBC, command=Memo_Pressed, text="Shutdown", align="right", height="6", width="fill", grid=[0,OBCSpacing])
+OBCmemo = PushButton(OBC, command=Memo_Pressed, text="                    Shutdown", align="right", height="6", width="fill", grid=[0,OBCSpacing])
 OBCmemo.bg = "black"
 OBCmemo.text_color = "white"
 TrackMode= PushButton(OBC, command=TrackMode_Pressed, text="TRACK", height="6", width="8", grid=[0,OBCSpacing])
