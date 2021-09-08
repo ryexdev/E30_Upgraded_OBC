bd56af6caaef309fc46fccf8f21f15de43b1edca
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2021-09-07 23:24:06 -0700

    Update V1-PZ_GP.py

--- V1-PZ_GP.py
+++ V1-PZ_GP.py
@@ -284,7 +284,7 @@ OBCSpacing = 1
 OBC = App(title="OBC", width=480, height=600, layout="grid")
 OBC.bg = "black"
 OBC.full_screen = True
-OBCMainText = Text(OBC, text = "Loading", font="digital-7", width=11, height="4", size=69, color="orange", grid=[0,OBCSpacing]);OBCSpacing += 1;
+OBCMainText = Text(OBC, text = "Loading", font="digital-7", width=11, height="3", size=69, color="orange", grid=[0,OBCSpacing]);OBCSpacing += 1;
 OBCMainText.bg = "#5E0000"
 OBCMainText.repeat(250, OBC_Data)
 spacer = Text(OBC, text="", grid=[0,OBCSpacing]);OBCSpacing += 1;
